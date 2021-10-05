e0188634ebeeb2dd2efca3a8f689beceed5e2c35
Fahmeda Hasan Fariha <58700965+mdjannatulhasan@users.noreply.github.com>
2021-10-05 12:13:54 +0600

    1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './public/**/*.js',
+  ],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {},
+   plugins: [],
+ }
